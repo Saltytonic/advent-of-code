2dba423b4196f3cc5161fcfe4cff9e335a53a695
Mason <mason.wooley@gmail.com>
2019-12-11 00:29:59 -0600

    Day 5 solutions

--- day-05/day-05-1.py
+++ day-05/day-05-1.py
@@ -1,48 +1,83 @@
 class IntcodeProcessor:
-    memory = []
-    inst_pntr = 0
+    __memory = []
+    __inst_ptr = 0
+    __running = False
+    __filename = ""
 
-    def load_memory(self, filename):
+    def set_instruction_file(self, filename):
+        self.__filename = filename
+
+    def __load_instruction_set(self):
         # Read from file and load into temp str array
-        fo = open(filename, "r")
+        fo = open(self.__filename, "r")
         temp = fo.readline().strip().split(",")
         fo.close()
         # Load str list into memory as ints
-        memory = [int(i) for i in temp]
+        self.__memory = [int(i) for i in temp]
 
-    def incr_pntr(self, count):
-        inst_pntr += count
+    def __incr_pointer(self, count):
+        self.__inst_ptr += count
+
+    def __read(self, addr, mode) -> int:
+        if mode == 1:
+            output = self.__memory[addr]
+            return output
+        else:
+            output = self.__memory[self.__memory[addr]]
+            return output
+
+    def __write(self, addr, value):
+        self.__memory[addr] = value
+
+    def __add_instruction(self, modes):
+        addr = self.__read(self.__inst_ptr+3,1)
+        val = self.__read(self.__inst_ptr+2,modes[1])+self.__read(self.__inst_ptr+1,modes[0])
+        self.__write(addr,val)
+        self.__incr_pointer(4)
+
+    def __multiply_instruction(self, modes):
+        addr = self.__read(self.__inst_ptr+3,1)
+        val = self.__read(self.__inst_ptr+2,modes[1])*self.__read(self.__inst_ptr+1,modes[0])
+        self.__write(addr,val)
+        self.__incr_pointer(4)
+
+    def __input_instruction(self):
+        addr = self.__read(self.__inst_ptr+1,1)
+        val = int(input("Input an integer: "))
+        self.__write(addr,val)
+        self.__incr_pointer(2)
+
+    def __output_instruction(self):
+        print("Output >>",self.__read(self.__inst_ptr+1,0))
+        self.__incr_pointer(2)
+
+    def __exit_instruction(self):
+        self.__running = False
+
+    def __process_instruction(self):
+        opcode = self.__read(self.__inst_ptr,1) % 100
+        modes = [((self.__read(self.__inst_ptr,1) // 10**n) % 10) for n in range(2,5)]
+
+        if opcode == 1:
+            self.__add_instruction(modes)
+        elif opcode == 2:
+            self.__multiply_instruction(modes)
+        elif opcode == 3:
+            self.__input_instruction()
+        elif opcode == 4:
+            self.__output_instruction()
+        elif opcode == 99:
+            self.__exit_instruction()
+        else:
+            print("Unknown instruction",opcode)
 
     def start(self):
-        
-while i < len(instr_list):
-    # Add instruction
-    if instr_list[i] == 1:
-        # Get addresses for opcode
-        ref_a = instr_list[i+1]
-        ref_b = instr_list[i+2]
-        out = instr_list[i+3]
-        # Perform add operation
-        instr_list[out] = instr_list[ref_a] + instr_list[ref_b]
-        # Step to next instruction
-        i += 4
-    # Multiply instruction
-    elif instr_list[i] == 2:
-        # Get addresses for opcode
-        ref_a = instr_list[i+1]
-        ref_b = instr_list[i+2]
-        out = instr_list[i+3]
-        # Perform multiply operation
-        instr_list[out] = instr_list[ref_a] * instr_list[ref_b]
-        # Step to next instruction
-        i += 4        
-    # Stop execution instruction
-    elif instr_list[i] == 99:
-        break
-    # Unknown instruction
-    else:
-        print("Unknown instruction")
+        self.__running = True
+        self.__load_instruction_set()
+        while self.__running:
+            self.__process_instruction()
 
-output = open("output.txt", "w")
-print(*instr_list, sep=",", file=output)
-output.close()
+
+int_cpu = IntcodeProcessor()
+int_cpu.set_instruction_file("input.txt")
+int_cpu.start()
--- day-05/day-05-2.py
+++ day-05/day-05-2.py
@@ -1,55 +1,117 @@
-fo = open("input.txt", "r")
-instr_list = fo.readline().strip().split(",")
-fo.close()
+class IntcodeProcessor:
+    __memory = []
+    __inst_ptr = 0
+    __running = False
+    __filename = ""
 
-# Convert all strings to ints in instruction list
-for i in range(0, len(instr_list)):
-    instr_list[i] = int(instr_list[i])
+    def set_instruction_file(self, filename):
+        self.__filename = filename
 
-init_instr_list = instr_list.copy()
-noun = 0
-verb = 0
-expected_output = 19690720
-found = False
-for i in range(0, 99):
-    if found:
-        break
-    for j in range(0, 99):
-        # Set initial conditions for program
-        ip = 0
-        instr_list = init_instr_list.copy()
-        instr_list[1] = i
-        instr_list[2] = j
-        while ip < len(instr_list):
-            # Add instruction
-            if instr_list[ip] == 1:
-                # Get addresses for opcode
-                ref_a = instr_list[ip+1]
-                ref_b = instr_list[ip+2]
-                out = instr_list[ip+3]
-                # Perform add operation
-                instr_list[out] = instr_list[ref_a] + instr_list[ref_b]
-                # Step to next instruction
-                ip += 4
-            # Multiply instruction
-            elif instr_list[ip] == 2:
-                # Get addresses for opcode
-                ref_a = instr_list[ip+1]
-                ref_b = instr_list[ip+2]
-                out = instr_list[ip+3]
-                # Perform multiply operation
-                instr_list[out] = instr_list[ref_a] * instr_list[ref_b]
-                # Step to next instruction
-                ip += 4        
-            # Stop execution instruction
-            elif instr_list[ip] == 99:
-                break
-            # Unknown instruction
-            else:
-                print("Unknown instruction")
-        if instr_list[0] == expected_output:
-            noun = i
-            verb = j
-            break
-    
-print(str(100 * noun + verb))
+    def __load_instruction_set(self):
+        # Read from file and load into temp str array
+        fo = open(self.__filename, "r")
+        temp = fo.readline().strip().split(",")
+        fo.close()
+        # Load str list into memory as ints
+        self.__memory = [int(i) for i in temp]
+
+    def __incr_pointer(self, count):
+        self.__inst_ptr += count
+
+    def __read(self, addr, mode) -> int:
+        if mode == 1:
+            output = self.__memory[addr]
+            return output
+        else:
+            output = self.__memory[self.__memory[addr]]
+            return output
+
+    def __write(self, addr, value):
+        self.__memory[addr] = value
+
+    def __add_instruction(self, modes):
+        addr = self.__read(self.__inst_ptr+3,1)
+        val = self.__read(self.__inst_ptr+2,modes[1])+self.__read(self.__inst_ptr+1,modes[0])
+        self.__write(addr,val)
+        self.__incr_pointer(4)
+
+    def __multiply_instruction(self, modes):
+        addr = self.__read(self.__inst_ptr+3,1)
+        val = self.__read(self.__inst_ptr+2,modes[1])*self.__read(self.__inst_ptr+1,modes[0])
+        self.__write(addr,val)
+        self.__incr_pointer(4)
+
+    def __input_instruction(self):
+        addr = self.__read(self.__inst_ptr+1,1)
+        val = int(input("Input an integer: "))
+        self.__write(addr,val)
+        self.__incr_pointer(2)
+
+    def __output_instruction(self):
+        print("Output >>",self.__read(self.__inst_ptr+1,0))
+        self.__incr_pointer(2)
+        
+    def __jump_if_true_instruction(self,modes):
+        if self.__read(self.__inst_ptr+1,modes[0]) != 0:
+            self.__inst_ptr = self.__read(self.__inst_ptr+2,modes[1])
+        else:
+            self.__incr_pointer(3)
+
+    def __jump_if_false_instruction(self,modes):
+        if self.__read(self.__inst_ptr+1,modes[0]) == 0:
+            self.__inst_ptr = self.__read(self.__inst_ptr+2,modes[1])
+        else:
+            self.__incr_pointer(3)
+
+    def __less_than_instruction(self,modes):
+        if self.__read(self.__inst_ptr+1,modes[0]) < self.__read(self.__inst_ptr+2,modes[1]):
+            self.__write(self.__read(self.__inst_ptr+3,1),1)
+        else:
+            self.__write(self.__read(self.__inst_ptr+3,1),0)
+        self.__incr_pointer(4)
+
+    def __equals_instruction(self,modes):
+        if self.__read(self.__inst_ptr+1,modes[0]) == self.__read(self.__inst_ptr+2,modes[1]):
+            self.__write(self.__read(self.__inst_ptr+3,1),1)
+        else:
+            self.__write(self.__read(self.__inst_ptr+3,1),0)
+        self.__incr_pointer(4)
+
+    def __exit_instruction(self):
+        self.__running = False
+
+    def __process_instruction(self):
+        opcode = self.__read(self.__inst_ptr,1) % 100
+        modes = [((self.__read(self.__inst_ptr,1) // 10**n) % 10) for n in range(2,5)]
+
+        if opcode == 1:
+            self.__add_instruction(modes)
+        elif opcode == 2:
+            self.__multiply_instruction(modes)
+        elif opcode == 3:
+            self.__input_instruction()
+        elif opcode == 4:
+            self.__output_instruction()
+        elif opcode == 5:
+            self.__jump_if_true_instruction(modes)
+        elif opcode == 6:
+            self.__jump_if_false_instruction(modes)
+        elif opcode == 7:
+            self.__less_than_instruction(modes)
+        elif opcode == 8:
+            self.__equals_instruction(modes)
+        elif opcode == 99:
+            self.__exit_instruction()
+        else:
+            print("Unknown instruction",opcode)
+
+    def start(self):
+        self.__running = True
+        self.__load_instruction_set()
+        while self.__running:
+            self.__process_instruction()
+
+
+int_cpu = IntcodeProcessor()
+int_cpu.set_instruction_file("input.txt")
+int_cpu.start()
